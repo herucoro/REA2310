1406c13002ab72827b9ad5895c3f97aa51e6820d
oomoto <oomoto@ryoei.jp>
2022-01-19 14:37:28 +0900

    update
    add controls

--- REA2310/MainForm.Designer.cs
+++ REA2310/MainForm.Designer.cs
@@ -34,12 +34,31 @@ namespace REA2300
             this.dateLabel = new System.Windows.Forms.Label();
             this.endBtn = new System.Windows.Forms.Button();
             this.printBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dateMtb
             // 
             this.dateMtb.Font = new System.Drawing.Font("MS UI Gothic", 14F);
-            this.dateMtb.Location = new System.Drawing.Point(304, 93);
+            this.dateMtb.Location = new System.Drawing.Point(202, 30);
             this.dateMtb.Mask = "##/##";
             this.dateMtb.Name = "dateMtb";
             this.dateMtb.Size = new System.Drawing.Size(100, 26);
@@ -51,7 +70,7 @@ namespace REA2300
             // 
             this.dateLabel.AutoSize = true;
             this.dateLabel.Font = new System.Drawing.Font("MS UI Gothic", 16F);
-            this.dateLabel.Location = new System.Drawing.Point(118, 97);
+            this.dateLabel.Location = new System.Drawing.Point(67, 34);
             this.dateLabel.Name = "dateLabel";
             this.dateLabel.Size = new System.Drawing.Size(98, 22);
             this.dateLabel.TabIndex = 1;
@@ -60,7 +79,7 @@ namespace REA2300
             // endBtn
             // 
             this.endBtn.Font = new System.Drawing.Font("MS UI Gothic", 14F);
-            this.endBtn.Location = new System.Drawing.Point(304, 179);
+            this.endBtn.Location = new System.Drawing.Point(386, 255);
             this.endBtn.Name = "endBtn";
             this.endBtn.Size = new System.Drawing.Size(100, 43);
             this.endBtn.TabIndex = 5;
@@ -71,25 +90,218 @@ namespace REA2300
             // printBtn
             // 
             this.printBtn.Font = new System.Drawing.Font("MS UI Gothic", 14F);
-            this.printBtn.Location = new System.Drawing.Point(116, 179);
+            this.printBtn.Location = new System.Drawing.Point(106, 255);
             this.printBtn.Name = "printBtn";
-            this.printBtn.Size = new System.Drawing.Size(100, 43);
+            this.printBtn.Size = new System.Drawing.Size(94, 43);
             this.printBtn.TabIndex = 3;
             this.printBtn.Text = "印刷";
             this.printBtn.UseVisualStyleBackColor = true;
             this.printBtn.Click += new System.EventHandler(this.printBtn_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.label1.Location = new System.Drawing.Point(67, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "決裁状態";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.radioButton1.Location = new System.Drawing.Point(202, 84);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 20);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "決裁";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.radioButton2.Location = new System.Drawing.Point(334, 84);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 20);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "未決裁";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.label2.Location = new System.Drawing.Point(67, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "入金種別";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 16F);
+            this.label3.Location = new System.Drawing.Point(67, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 22);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "銀行";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox1.Location = new System.Drawing.Point(202, 125);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 20);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "全て";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(17, 4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(58, 20);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "手形";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(103, 3);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(58, 20);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "電債";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(178, 3);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(90, 20);
+            this.checkBox4.TabIndex = 14;
+            this.checkBox4.Text = "期日指定";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(285, 4);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(101, 20);
+            this.checkBox5.TabIndex = 15;
+            this.checkBox5.Text = "ファクタリング";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox10.Location = new System.Drawing.Point(202, 186);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(55, 20);
+            this.checkBox10.TabIndex = 16;
+            this.checkBox10.Text = "全て";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox4);
+            this.panel1.Controls.Add(this.checkBox5);
+            this.panel1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.panel1.Location = new System.Drawing.Point(185, 147);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(389, 33);
+            this.panel1.TabIndex = 21;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBox6);
+            this.panel2.Controls.Add(this.checkBox7);
+            this.panel2.Controls.Add(this.checkBox8);
+            this.panel2.Controls.Add(this.checkBox9);
+            this.panel2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.panel2.Location = new System.Drawing.Point(185, 208);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(389, 30);
+            this.panel2.TabIndex = 22;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(17, 4);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(58, 20);
+            this.checkBox6.TabIndex = 12;
+            this.checkBox6.Text = "豊信";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(103, 4);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(54, 20);
+            this.checkBox7.TabIndex = 13;
+            this.checkBox7.Text = "UFJ";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(178, 4);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(58, 20);
+            this.checkBox8.TabIndex = 14;
+            this.checkBox8.Text = "碧信";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(259, 4);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(90, 20);
+            this.checkBox9.TabIndex = 15;
+            this.checkBox9.Text = "三井住友";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 322);
+            this.ClientSize = new System.Drawing.Size(611, 322);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.printBtn);
             this.Controls.Add(this.endBtn);
             this.Controls.Add(this.dateLabel);
             this.Controls.Add(this.dateMtb);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +313,23 @@ namespace REA2300
         private System.Windows.Forms.Label dateLabel;
         private System.Windows.Forms.Button endBtn;
         private System.Windows.Forms.Button printBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
     }
 }
 
