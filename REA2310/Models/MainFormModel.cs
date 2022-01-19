7a8c78bc7be531ad4daf5da60183bcb52aac829f
oomoto <oomoto@ryoei.jp>
2022-01-19 18:02:00 +0900

    add
    main form control's model

---
+++ REA2310/Models/MainFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REA2310.Models
+{
+    public class MainFormModel
+    {
+        public string date;
+        public bool approvalType;
+        public bool[] paymentType = new bool[5];
+        public bool[] bankType = new bool[5];
+
+    }
+}
