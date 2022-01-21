3c5312add2d8ba3dd4fd285bd75ac819d1bfcff0
oomoto <oomoto@ryoei.jp>
2022-01-21 19:05:38 +0900

    add
    created to use print data item

---
+++ REA2310/Models/PrintModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REA2310.Models
+{
+    class PrintModel
+    {
+
+        public string yyyyMM;
+        public string dd;
+        public int supplierCode;
+        public string paymentName;
+        public string bankName;
+        public string amount;
+    }
+}
